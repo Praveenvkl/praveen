556727e26df8a97b1feb742834a0d8bdbea21579
Praveenvkl <159679362+Praveenvkl@users.noreply.github.com>
2024-07-15 09:13:49 +0530

    Loop.java

---
+++ Loop.java
@@ -0,0 +1,7 @@
+public class public class Loop100to0{ 
+    public static void main(String[] args) {
+            for(int i=100;i>=0;i--){
+                System.out.print(i+" ");
+            }
+        }
+    }
