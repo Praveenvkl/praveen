ba45f223ce36af4a7671beb8e5aa13652bd03195
Praveenvkl <159679362+Praveenvkl@users.noreply.github.com>
2024-07-15 09:15:42 +0530

    LoopSkip2Digit.java

---
+++ LoopSkip2Digit.java
@@ -0,0 +1,7 @@
+public class LoopSkip2Digit{ 
+    public static void main(String[] args) {
+            for(int i=100;i>=0;i=i-3){
+                System.out.print(i+" ");
+            }
+        }
+    }
